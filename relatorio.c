1a70b5c1d27e87266951d9fb50ef349218e8bd0e
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2024-10-07 11:37:28 -0300

    Modulariza módulo de relatórios

---
+++ relatorio.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "relatorio.h"
+
+// Funções
+
+char menu_relatorios(void) {
+    char opc;
+    system("clear||cls");
+    printf("\n");
+    printf("|==================================================================|\n");
+    printf("|===============|          Menu Relatórios         |===============|\n");
+    printf("|==================================================================|\n");
+    printf("|=====|                                                      |=====|\n");
+    printf("|=====|                [1] Relatório Comandas                |=====|\n");
+    printf("|=====|                [2] Relatório Estoque                 |=====|\n");
+    printf("|=====|                [3] Relatório Garçom                  |=====|\n");
+    printf("|=====|                [0] Retornar ao Menu Principal        |=====|\n");
+    printf("|=====|                                                      |=====|\n");
+    printf("|==================================================================|\n");
+    printf("|=====| Escolha uma opção: ");
+    scanf("%c", &opc);
+    getchar();
+    printf("|==================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
+
+
+void relatorio_comandas(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("|=====================================================================|\n");
+    printf("|===============|          Relatório Comandas         |===============|\n");
+    printf("|=====================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
+
+
+void relatorio_estoque(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("|=====================================================================|\n");
+    printf("|===============|          Relatório Estoque          |===============|\n");
+    printf("|=====================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
+
+
+void relatorio_garcom(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("|=====================================================================|\n");
+    printf("|===============|          Relatório Garçom           |===============|\n");
+    printf("|=====================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
---
+++ relatorio.h
@@ -0,0 +1,6 @@
+// Assinatura das funções de relatório
+
+char menu_relatorios(void);
+void relatorio_comandas(void);
+void relatorio_estoque(void);
+void relatorio_garcom(void);
