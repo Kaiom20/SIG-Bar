f7a1c3c0f9b3b8984d40c57a0842cee98332fe6f
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-05 19:51:31 -0300

    Altera as funções validar CPF

--- valida.c
+++ valida.c
@@ -35,55 +35,64 @@ int valida_nome(char * nome){
     return 1;
 }
 
-//Validação de CPF (Créditos: ChatGPT)
-int digitos_iguais(char * cpf) {
-    for (int i = 1; i < 11; i++) {
-        if (cpf[i] != cpf[0]) {
-            return 0;
-        }
+//Validação de CPF (Créditos: Samuel Morais/SIG-Beer: https://github.com/SamuelMorais45)
+int validarFormatoCPF(const char *cpf) {
+ 
+    if (strlen(cpf) != 14) return 0;
+    for (int i = 0; i < 14; i++) {
+        if ((i == 3 || i == 7) && cpf[i] != '.') return 0;    
+        if (i == 11 && cpf[i] != '-') return 0;               
+        if ((i != 3 && i != 7 && i != 11) && !isdigit(cpf[i])) return 0; 
     }
+   
     return 1;
 }
 
-int valida_CPF(char * cpf) {
 
-    if (strlen(cpf) != 11) {
-        return 0;
-    }
+int validarDigitosCPF(const char *cpf) {
+    int soma = 0, resto;
+    int numeros[11];
+   
     
-    for (int i = 0; i < 11; i++) {
-        if (!isdigit(cpf[i])) {
-            return 0;
+    int j = 0;
+    for (int i = 0; i < 14; i++) {
+        if (isdigit(cpf[i])) {
+            numeros[j] = cpf[i] - '0';
+            j++;
         }
     }
-    if (digitos_iguais(cpf)) {
-        return 0;
-    }
 
-    int soma = 0;
+   
     for (int i = 0; i < 9; i++) {
-        soma += (cpf[i] - '0') * (10 - i);
+        soma += numeros[i] * (10 - i);
     }
-    int primeiroDigitoVerificador = (soma * 10) % 11;
-    if (primeiroDigitoVerificador == 10) {
-        primeiroDigitoVerificador = 0;
-    }
-    if (primeiroDigitoVerificador != (cpf[9] - '0')) {
-        return 0;
-    }
+    resto = (soma * 10) % 11;
+    if (resto == 10) resto = 0;
+    if (resto != numeros[9]) return 0;
 
+ 
     soma = 0;
     for (int i = 0; i < 10; i++) {
-        soma += (cpf[i] - '0') * (11 - i);
+        soma += numeros[i] * (11 - i);
     }
-    int segundoDigitoVerificador = (soma * 10) % 11;
-    if (segundoDigitoVerificador == 10) {
-        segundoDigitoVerificador = 0;
-    }
-    if (segundoDigitoVerificador != (cpf[10] - '0')) {
-        return 0; 
-    }
+    resto = (soma * 10) % 11;
+    if (resto == 10) resto = 0;
+    if (resto != numeros[10]) return 0;
 
+    return 1;
+}
+
+int validarCPF(const char *cpf) {
+    if (!validarFormatoCPF(cpf)) {
+        printf("Formato de CPF inválido.\n");
+        return 0;
+    }
+   
+    if (!validarDigitosCPF(cpf)) {
+        printf("Dígitos verificadores inválidos.\n");
+        return 0;
+    }
+   
     return 1;
 }
 
