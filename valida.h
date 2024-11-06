6732292ae43ac89833223947c01ef2c091decfa9
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-06 07:25:00 -0300

    Atualiza valida.h

--- valida.h
+++ valida.h
@@ -2,8 +2,9 @@
 
 int valida_fone(char*);
 int valida_nome(char*);
-int digitos_iguais(char*);
-int valida_CPF(char*);
+int validarFormatoCPF(const char*);
+int validarDigitosCPF(const char*);
+int validarCPF(const char*);
 int bissexto(char year[]);
 int dia_mes(int mes);
 int validar_data(char day[], char month[], char year[]);
