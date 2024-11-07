e17a549723bf44c404b20e2db61c22aa5b8a8c24
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-07 14:46:23 -0300

    Cria função ler_CPF

--- ler_garcom.c
+++ ler_garcom.c
@@ -5,13 +5,18 @@
 // funções
 
 
-void ler_nome(char *nome) {
+void ler_nome(char * nome) {
     printf("|=====|         Nome: ");
     scanf("%[A-ZÁÉÍÓÚÃÕÂÊÎÔÛÀÇ a-záéíóúãõâêîôûàç]", nome);
 }
 
 
-void ler_nasc(char *nasc) {
+void ler_nasc(char * nasc) {
     printf("|=====|         Data de nascimento: ");
     scanf("%[0-9/]", nasc);
+}
+
+void ler_cpf(char * cpf){
+    printf("|=====|         CPF: ");
+    scanf("%[0-9.-]", cpf);
 }
