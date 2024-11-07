6010cf0a4516b1e279a23f2fefe76bce6966b550
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-07 15:57:14 -0300

    Cria função para ler o ID

--- ler_estoque.c
+++ ler_estoque.c
@@ -5,7 +5,15 @@
 
 // Funções
 
-void ler_id(char *idprod) {
+void ler_idprod(char *idprod) {
     printf("|=====|               ID do Produto: ");
     scanf("%[0-9]", idprod);
+    getchar();
+}
+
+
+void ler_nomeProd(char *nome) {
+    printf("|=====|               Nome: ");
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+    getchar();
 }
