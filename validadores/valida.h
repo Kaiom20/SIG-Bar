36c21c4c6ab77443e2a7b8f56f016f808954ce87
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-18 16:31:06 -0300

    colocando os arquivos na sua pasta

---
+++ validadores/valida.h
@@ -0,0 +1,15 @@
+//Assinatura das funções de validação
+
+#ifndef VALIDA
+#define VALIDA
+
+int valida_fone(char*);
+int valida_nome(char*);
+int validarFormatoCPF(const char*);
+int validarDigitosCPF(const char*);
+int validarCPF(const char*);
+int bissexto(char year[]);
+int dia_mes(int mes);
+int validar_data(char day[], char month[], char year[]);
+
+#endif
