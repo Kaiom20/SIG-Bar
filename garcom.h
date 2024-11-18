52732530b2cf45e9f79ced7830e484e7d1c59d6c
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-18 16:28:47 -0300

    Removendo esses arquivos para pastas

--- garcom.h
+++
@@ -1,8 +0,0 @@
-// Assinatura das funções de garçom
-
-void modulo_garcom(void);
-char menu_garcom(void);
-void cadastrar_garcom(void);
-void exibir_garcom(void);
-void alterar_garcom(void);
-void excluir_garcom(void);
