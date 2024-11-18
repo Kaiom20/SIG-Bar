b63ecd7dc00002e44c7ce1c7edd41ac70eb21856
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-18 16:29:42 -0300

    Colocando os arquivos de leitura de garçom na sua pasta

---
+++ garcom/ler_garcom.h
@@ -0,0 +1,12 @@
+// Assinatura das funções
+
+#ifndef LER_GARCOM
+#define LER_GARCOM
+
+void ler_nome(char *nome);
+void ler_nasc(char *nasc);
+void ler_cpf(char *cpf);
+void ler_fone(char *telefone);
+void ler_idgar(char * id_garcom);
+
+#endif
