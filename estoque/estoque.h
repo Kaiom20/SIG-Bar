243a6279f06bdda94315b916138a520fb7db7563
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-19 09:25:33 -0300

    Atualiza estoque.h

--- estoque/estoque.h
+++ estoque/estoque.h
@@ -1,8 +1,13 @@
 // Assinatura das funções de estoque
 
+#ifndef ESTOQUE
+#define ESTOQUE
+
 void modulo_estoque(void);
 char menu_estoque(void);
 void cadastrar_produto(void);
 void exibir_produto(void);
 void alterar_produto(void);
 void excluir_produto(void);
+
+#endif
