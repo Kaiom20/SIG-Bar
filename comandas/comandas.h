7642085eeda0876ad37b19799988879621be4180
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-19 09:18:15 -0300

    Atualiza comandas.h

--- comandas/comandas.h
+++ comandas/comandas.h
@@ -1,8 +1,13 @@
 // Assinatura das funções de comandas
 
+#ifndef COMANDAS
+#define COMANDAS
+
 void modulo_comanda(void);
 char menu_comandas(void);
 void cadastrar_comanda(void);
 void exibir_comanda(void);
 void alterar_comanda(void);
 void excluir_comanda(void);
+
+#endif
