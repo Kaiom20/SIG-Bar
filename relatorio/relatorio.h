2342fabfd848ffc04a8522b6e4e5977d3d44817a
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-19 09:21:58 -0300

    Atualiza relatorio.h

--- relatorio/relatorio.h
+++ relatorio/relatorio.h
@@ -1,7 +1,12 @@
 // Assinatura das funções de relatório
 
+#ifndef RELATORIO
+#define RELATORIO
+
 void modulo_relatorio(void);
 char menu_relatorios(void);
 void relatorio_comandas(void);
 void relatorio_estoque(void);
 void relatorio_garcom(void);
+
+#endif
