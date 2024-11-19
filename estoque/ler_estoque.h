aa99a398ae82e77d4f41bd59e0ae4cb682c1a469
Fillipe Medeiros <fillipemdrs.63@gmail.com>
2024-11-19 09:27:06 -0300

    Atualiza ler_estoque.h

--- estoque/ler_estoque.h
+++ estoque/ler_estoque.h
@@ -1,7 +1,12 @@
-// Assinatura das funçõesz
+// Assinatura das funções
+
+#ifndef LER_ESTOQUE
+#define LER_ESTOQUE
 
 void ler_idprod(char *idprod);
 void ler_nomeProd(char *nome);
 void ler_validade(char *valid);
 void ler_quanti(char *quant);
 void ler_valor(char *valor);
+
+#endif
