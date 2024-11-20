156904a6b479d7bccea0655aadec993be5206ae9
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2024-11-20 12:48:14 -0300

    Criando o arquivo para a função do módulo de navegação isoladamente

---
+++ garcom/modulo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "modulo.h"
+#include "garcom.h"
+
+// Função do módulo garçom
+
+void modulo_garcom(void) {
+    
+    char opcao;
+
+    do {
+        opcao = menu_garcom();
+
+        switch(opcao) {
+            case '1': cadastrar_garcom();
+                        break;                      
+            case '2': exibir_garcom();
+                        break;
+            case '3': alterar_garcom();
+                        break;
+            case '4': excluir_garcom();
+                        break;
+        }
+
+    } while (opcao != '0');
+}
---
+++ garcom/modulo.h
@@ -0,0 +1,8 @@
+// Assinatura das funções
+
+#ifndef MODULO
+#define MODULO
+
+void modulo_garcom(void);
+
+#endif
