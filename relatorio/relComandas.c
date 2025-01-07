716007c79fc4a1ffab2696f61734d1089bd4f8b6
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-01-06 22:09:11 -0300

    cria um arquivo exclusivo para o relatorio de comandas

---
+++ relatorio/relComandas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "relComandas.h"
+
+// Funções
+
+void relatorio_comandas(void) {          //sem funcionalidade no momento
+    system("clear||cls");
+    printf("\n");
+    printf("|=====================================================================|\n");
+    printf("|===============|          Relatório Comandas         |===============|\n");
+    printf("|=====================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
---
+++ relatorio/relComandas.h
@@ -0,0 +1,8 @@
+// Assinatura das funções do relatório das comandas
+
+#ifndef RELCOMANDAS
+#define RELCOMANDAS
+
+void relatorio_comandas(void);
+
+#endif
