1cee12f7df175a8613b3e1a9e945eac8913bfe96
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-01-07 22:45:44 -0300

    cria um novo arquivo somente para o relatório de garcom

---
+++ relatorio/relGarcom.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "relGarcom.h"
+
+// Funções
+
+void relatorio_garcom(void) {          //sem funcionalidade no momento
+    system("clear||cls");
+    printf("\n");
+    printf("|=====================================================================|\n");
+    printf("|===============|          Relatório Garçom           |===============|\n");
+    printf("|=====================================================================|\n");
+    printf("\n");
+    printf("\t >>>  Pressione <ENTER> para continuar  <<<");
+    getchar();
+}
