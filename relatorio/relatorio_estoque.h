82c11f827c78426f42b015a6b5c379c2133ced7c
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-01-22 23:02:11 -0300

    concertando bugs

---
+++ relatorio/relatorio_estoque.h
@@ -0,0 +1,12 @@
+// Assinatura das funções do relatório do estoque
+
+#ifndef RELATORIO_ESTOQUE
+#define RELATORIO_ESTOQUE
+
+void relatorio_estoque(void);
+char menu_relatorio_estoque(void);
+void E_geral(void);
+void E_ativos(void);
+void E_inativos(void);
+
+#endif
